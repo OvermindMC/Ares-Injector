80c9e1f4a7dc78620c4ad53fe194aded2ad131d6
OvermindMC <Overmind@mail.com>
2023-09-08 20:48:49 +0100

    Update Utils.h

--- Ares Injector/Utils/Utils.h
+++ Ares Injector/Utils/Utils.h
@@ -3,14 +3,20 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
 #include <WinSock2.h>
+
 #pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "urlmon.lib")
 
 #include <filesystem>
+#include <TlHelp32.h>
 #include <Windows.h>
+#include <algorithm>
 #include <iostream>
+#include <codecvt>
 #include <ostream>
 #include <fstream>
 #include <string>
+#include <locale>
 
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
@@ -25,4 +31,7 @@ public:
 public:
 	static auto storeToken(const char*) -> void;
 	static auto getToken(void) -> std::string;
+public:
+	static auto isAresInjected(void) -> bool;
+	static auto injectLatest(void) -> void;
 };
